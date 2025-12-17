03fc4167af914f6e810685403c7fd5e4f2868785
SoundaryaShanmugam <soundarya.shanmugam@aspiresys.com>
2025-12-17 11:18:50 +0530

    Initial commit with Figma design documents and functional requirements

--- ApiGateway/obj/Debug/net9.0/ApiGateway.AssemblyInfo.cs
+++ ApiGateway/obj/Debug/net9.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ab85628e9c6ba6f538715c352a195a61dcac817")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
