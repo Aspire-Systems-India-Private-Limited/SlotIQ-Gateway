a271442e73d9e50b0132045139082eaed795a761
SoundaryaShanmugam <soundarya.shanmugam@aspiresys.com>
2025-12-18 16:09:50 +0530

    Gaeway Changes

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -6,9 +6,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json");
 
 builder.Services.AddOcelot();
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ApiCorsPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000","https://localhost:3000") // Allow the frontend origin
+              .AllowAnyMethod() // Allow all HTTP methods
+              .AllowAnyHeader() // Allow all headers
+              .AllowCredentials(); // Allow cookies/auth headers
+    });
+});
 
 var app = builder.Build();
 
-await app.UseOcelot();
+// IMPORTANT: CORS must be added BEFORE Ocelot
+app.UseCors("ApiCorsPolicy");
 
+await app.UseOcelot();
+ 
 app.Run();
